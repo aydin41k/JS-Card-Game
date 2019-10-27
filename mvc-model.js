c9d47beeffb3e17f080f5930b097473ca34374e7
smolindamix <yannlfphoto@gmail.com>
2019-10-27 11:14:19 +0100

    The queen is in the workflow now

--- mvc-model.js
+++ mvc-model.js
@@ -2,8 +2,8 @@ var userScore = 0;
 var computerScore = 0;
 
 var suit = ['hearts','diamonds','clubs','spades'];
-var type = ['2','3','4','5','6','7','8','9','10','jack','king','ace'];
-var value = [2,3,4,5,6,7,8,9,10,11,13,14];
+var type = ['2','3','4','5','6','7','8','9','10','jack','queen','king','ace'];
+var value = [2,3,4,5,6,7,8,9,10,11,12,13,14];
 
 var cardTypeCount = type.length;
 var randCache;
